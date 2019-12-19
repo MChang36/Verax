fb4b11f7eae5757629aaaf4abad06a43848f70be
21chang <matthew36chang@da.org>
2019-12-19 12:56:47 -0500

    Added models and views. Created app/urls.py

--- veraproj/verax/models.py
+++ veraproj/verax/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+
+class Student(models.Model):
+    name = models.CharField(max_length=250)
+    average = models.DecimalField(decimal_places=2, max_digits=5, null=True, blank=True)
+
+    # irrelevent
+    def changeAverage(self, amt):
+        if self.average is None:
+            print("true")
+            self.average = amt
+            self.save(update_fields=['average'])
+        else:
+            print("false")
+            self.average = (self.average + amt)/2
+            self.save(update_fields=['average'])
+
+    #used
+    def calculated_ave(self):
+        my_tests = Test.objects.all().filter(student=self)
+        print(my_tests)
+        sum = 0
+        for test in my_tests:
+            sum += test.grade
+        ave = sum / my_tests.count()
+        return ave
+
+
+    def __str__(self):
+        return self.name
+
+#irrelevent
+def updateAverage(instance, created, **kwargs):
+    if created:
+        student = instance.student
+        print('hello')
+        student.changeAverage(instance.grade)
+
+class Test(models.Model):
+    name = models.CharField(max_length=250)
+    student = models.ForeignKey(Student, null=True, on_delete=models.CASCADE)
+    grade = models.PositiveIntegerField()
+
+    def __str__(self):
+        return "{}-{}".format(self.student, self.name)
+
+#irrelevent
+post_save.connect(updateAverage, sender=Test)
--- veraproj/verax/views.py
+++ veraproj/verax/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.urls import reverse
 
+from .models import Student, Test
+from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 # Create your views here.
+
+class StudentList(ListView):
+    model = Student
+    context_object_name = 'students'
+    template_name = "studentlist.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        return context
+
+class TestList(ListView):
+    model = Test
+    context_object_name = 'tests';
+    template_name = "testlist.html"
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        return context
+
+class TestCreate(CreateView):
+    model = Test
+    fields = ['name', 'student', 'grade']
+    template_name = "test_form.html"
+
+    def form_valid(self, form):
+        form.instance.created_by = self.request.user
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('students')
